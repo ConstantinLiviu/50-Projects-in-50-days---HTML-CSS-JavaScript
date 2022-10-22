3837d6d1f95e1f15e6d20d15fc1c00172687d4c8
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-10-22 22:24:21 +0300

    mobile navbar added

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const homeMenuIcon = document.querySelector(".ham-menu");
+const navList = document.querySelector("nav ul");
 
+homeMenuIcon.addEventListener("click", () => {
+  navList.classList.toggle("hide-ul");
+});
