ec8e041955561b2bacc78c6534f7856b8247f4af
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-10-23 01:03:12 +0300

    updated structure, added styles, addded functionality

--- Day 5 - Blurry loading/script.js
+++ Day 5 - Blurry loading/script.js
@@ -1,1 +1,24 @@
-alert("merge!");
+const imageEl = document.querySelector(".img");
+const textEl = document.querySelector(".text");
+const refreshBtn = document.querySelector(".refresh");
+
+let percentage = -1;
+let int = setInterval(increaseBlur, 40);
+
+function increaseBlur() {
+  percentage++;
+  if (percentage > 99) {
+    clearInterval(int);
+  }
+  textEl.innerHTML = `${percentage}%`;
+  imageEl.style.filter = `blur(${100 - percentage}px)`;
+  textEl.style.opacity = `${1.2 - percentage / 100}`;
+  if (textEl.innerHTML == "100%") {
+    textEl.style.display = "none";
+    refreshBtn.style.display = "block";
+  }
+}
+
+refreshBtn.addEventListener("click", () => {
+  window.location.reload();
+});
