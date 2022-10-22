4c5e126b658596564dc9f33f00fce127d5d50047
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-10-22 17:23:29 +0300

    navigation readme btn and modal pop-up completed

--- Day 3 - Rotating navigation/script.js
+++ Day 3 - Rotating navigation/script.js
@@ -3,6 +3,9 @@ const closeEl = document.getElementById("close");
 const navigationCircle = document.querySelector(".navigation");
 const pageEl = document.querySelector(".page");
 const menuEl = document.querySelector(".menu");
+const readmeBtn = document.querySelector(".readmebtn");
+const overlayEl = document.querySelector(".overlay");
+const modalEl = document.querySelector(".modal-readme");
 
 openEl.addEventListener("click", () => {
   navigationCircle.classList.add("rotate");
@@ -14,4 +17,14 @@ closeEl.addEventListener("click", () => {
   navigationCircle.classList.remove("rotate");
   pageEl.classList.remove("tilt-page");
   menuEl.style.left = "-20rem";
+});
+
+readmeBtn.addEventListener("click", () => {
+  overlayEl.style.display = "block";
+  modalEl.style.display = "block";
+});
+
+overlayEl.addEventListener("click", () => {
+  overlayEl.style.display = "none";
+  modalEl.style.display = "none";
 });
