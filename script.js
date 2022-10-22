3116b4ef3498bebd94dbe33080b9df6bc723ad05
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-10-22 15:23:55 +0300

    moved modal code to its own file

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const modalBtn = document.querySelector(".readme");
-const overlayEl = document.querySelector(".overlay");
-const modalEl = document.querySelector(".modal-readme");
 
-modalBtn.addEventListener("click", () => {
-  modalEl.style.display = "block";
-  overlayEl.style.display = "block";
-});
-
-overlayEl.addEventListener("click", () => {
-  modalEl.style.display = "none";
-  overlayEl.style.display = "none";
-});
