03e212d4e54e3ebc2ecd0897dfa69eb36dd3a303
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-10-22 22:58:23 +0300

    project 4 mobile navbar

--- Day 4 - Hidden Search Widget/script.js
+++ Day 4 - Hidden Search Widget/script.js
@@ -1,5 +1,11 @@
 const searchBtn = document.querySelector("button");
 const inputEl = document.querySelector("input");
+const homeMenuIcon = document.querySelector(".ham-menu");
+const navList = document.querySelector("nav ul");
+
+homeMenuIcon.addEventListener("click", () => {
+  navList.classList.toggle("hide-ul");
+});
 
 searchBtn.addEventListener("click", () => {
   inputEl.classList.toggle("active");
