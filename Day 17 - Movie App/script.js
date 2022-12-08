d69c5a16be13854d3266fd83e7cacd547cb6b1b1
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-12-08 19:56:36 +0200

    fixed poster image api link, added a function that determines rating span color class, added a class that retreives movies info and renders movie cards on page

--- Day 17 - Movie App/script.js
+++ Day 17 - Movie App/script.js
@@ -1,14 +1,14 @@
 const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c9a963aede0726fe0999645ae907bd0d&page=1";
 
-const IMG_PATH =
-  "https://image.tmdb.org/t/p/w500/bQXAqRx2Fgc46uCVWgoPz5L5Dtr.jpg";
+const IMG_PATH = "https://image.tmdb.org/t/p/w500/";
 
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=c9a963aede0726fe0999645ae907bd0d&query="';
 
 const formEl = document.querySelector(".form");
 const searchEl = document.querySelector(".search");
+const moviesListEl = document.querySelector(".movies-list");
 
 /**
  * Function takes in an api url key in order to retrieve movie related info
@@ -19,8 +19,59 @@ async function getMovies(url) {
   const response = await fetch(url);
   const data = await response.json();
 
-  //   console.log(data.results[0]["overview"]);
-  console.log(data.results);
+  showMovies(data.results);
+}
+
+/**
+ * Takes in the page search results as an objects and populates the page with movie cards based on search terms relevance
+ * @param {Object} movieData
+ */
+function showMovies(movieData) {
+  // clear any existing movie/page elements
+  moviesListEl.innerHTML = "";
+
+  // iterate through search results
+  movieData.forEach((movie) => {
+    //destructure retrieved data
+    const { title, poster_path, vote_average, overview } = movie;
+
+    // create empty element on page for each item
+    const movieEl = document.createElement("div");
+    // add styles
+    movieEl.classList.add("movie-item");
+    // assign movie item structure
+    movieEl.innerHTML = `
+    <img src="${IMG_PATH + poster_path}" alt="${title}" class="movie-poster" />
+    <div class="movie-info">
+      <h3 class="movie-title">${title}</h3>
+      <span class="rating ${avgVoteToRating(
+        vote_average
+      )}">${vote_average}</span>
+    </div>
+    <div class="synopsis">
+      <h3 class="synopsis-title">Synopsis</h3>
+      <p class="synopsis-text">
+        ${overview}
+      </p>
+    </div>`;
+
+    moviesListEl.appendChild(movieEl);
+  });
+}
+
+/**
+ * function the vote_average param to determine which rating class is applied by returning the class as a string
+ * @param {string} voteResult
+ * @returns
+ */
+function avgVoteToRating(voteResult) {
+  if (voteResult >= 8) {
+    return "good";
+  } else if (voteResult >= 5) {
+    return "meh";
+  } else {
+    return "bad";
+  }
 }
 
 getMovies(API_URL);
