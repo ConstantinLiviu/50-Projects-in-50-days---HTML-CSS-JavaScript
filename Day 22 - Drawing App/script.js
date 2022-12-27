f86ed441983029491d5b1ace85cb8cfaf38e3f86
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-12-27 13:01:48 +0200

    canvas requires additional study, project currently on hold

--- Day 22 - Drawing App/script.js
+++ Day 22 - Drawing App/script.js
@@ -1,1 +1,14 @@
-alert("merge!");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+let size = 20;
+let color = "black";
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+drawCircle(40, 40);
