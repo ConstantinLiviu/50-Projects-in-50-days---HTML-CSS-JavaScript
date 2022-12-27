72a056233f383033ae29ff904c380784bd8c9006
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-12-27 15:39:04 +0200

    created elements list

--- Day 24 - Content Placeholder/script.js
+++ Day 24 - Content Placeholder/script.js
@@ -1,1 +1,6 @@
-alert("merge!");
+const imgContainerEl = document.querySelector(".main-img-container");
+const titleEl = document.querySelector(".title-container");
+const descriptionEl = document.querySelector(".description");
+const profilePicEl = document.querySelector(".profile-pic-container");
+const idNameEl = document.querySelector(".name");
+const occupationEl = document.querySelector(".occupation");
