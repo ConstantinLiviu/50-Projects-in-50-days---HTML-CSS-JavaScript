beb5429ef38db5419b30da703ec4e3af809fba10
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2022-12-31 20:43:12 +0200

    updated functionality to allow color/background color changes based on the text

--- Day 27 - Toast Notifications/script.js
+++ Day 27 - Toast Notifications/script.js
@@ -8,6 +8,13 @@ const notificationsMsg = [
   "Discount code applied!",
 ];
 
+const notificationsCol = [
+  ["#fb3737", "#FFE6E6"],
+  ["#48a953", "#deefce"],
+  ["#354259", "#DAEAF1"],
+  ["#85586F", "#FDF6EC"],
+];
+
 btnEl.addEventListener("click", () => {
   // creates an element to be used as the notification body
   const notification = document.createElement("p");
@@ -16,9 +23,30 @@ btnEl.addEventListener("click", () => {
   notification.classList.add("notification");
 
   // randomly adds a preset notification text
-  notification.innerText = `${
-    notificationsMsg[Math.floor(Math.random() * notificationsMsg.length)]
-  }`;
+  let randomIndex = Math.floor(Math.random() * notificationsMsg.length);
+  notification.innerText = `${notificationsMsg[randomIndex]}`;
+
+  // change background and color depending on the message
+  switch (randomIndex) {
+    case 0:
+      notification.style.color = `${notificationsCol[0][0]}`;
+      notification.style.backgroundColor = `${notificationsCol[0][1]}`;
+      break;
+    case 1:
+      notification.style.color = `${notificationsCol[1][0]}`;
+      notification.style.backgroundColor = `${notificationsCol[1][1]}`;
+      break;
+    case 2:
+      notification.style.color = `${notificationsCol[2][0]}`;
+      notification.style.backgroundColor = `${notificationsCol[2][1]}`;
+      break;
+    case 3:
+      notification.style.color = `${notificationsCol[3][0]}`;
+      notification.style.backgroundColor = `${notificationsCol[3][1]}`;
+      break;
+    default:
+      console.log("ceva gresit");
+  }
 
   // adds notification element as a child of the notifications container
   notificationsCont.appendChild(notification);
