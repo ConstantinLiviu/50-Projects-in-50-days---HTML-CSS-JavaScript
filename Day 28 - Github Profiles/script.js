6da39fdf838a90a8a3fbac3a116b079ee0367e56
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-01 16:59:55 +0200

    API fetch request working

--- Day 28 - Github Profiles/script.js
+++ Day 28 - Github Profiles/script.js
@@ -1,1 +1,32 @@
-alert("merge!");
+const APIURL = "https://api.github.com/users/";
+const formEl = document.querySelector("form");
+const searchEl = document.querySelector(".searchbar");
+
+/**
+ * Function takes in the username in order to return info from their Github profile
+ * @param {string} username
+ */
+async function getUser(username) {
+  try {
+    // destructure response object to get just the 'data' property
+    const { data } = await axios(APIURL + username);
+
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createUserCard(user) {}
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const user = searchEl.value;
+
+  if (user) {
+    getUser(user);
+
+    searchEl.value = "";
+  }
+});
