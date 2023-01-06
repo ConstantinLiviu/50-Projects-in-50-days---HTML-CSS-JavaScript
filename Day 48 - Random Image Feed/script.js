d6349019c390a4f75b36062a513010ca6d7ecdbc
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-06 03:20:27 +0200

    added functionality

--- Day 48 - Random Image Feed/script.js
+++ Day 48 - Random Image Feed/script.js
@@ -1,1 +1,19 @@
-alert("merge!");
+const containerEl = document.querySelector(".img-container");
+const imgURL = "https://source.unsplash.com/random/";
+const imgTotal = 15;
+
+for (i = 0; i < imgTotal; i++) {
+  const img = document.createElement("img");
+  img.src = `${imgURL}${imgSize()}`;
+  containerEl.appendChild(img);
+}
+
+/**
+ * Randomly generates width and height to be used as image size
+ * @returns sets the size of the image that's being displayed
+ */
+function imgSize() {
+  return `${Math.floor(Math.random() * 10) + 300}x${
+    Math.floor(Math.random() * 10) + 300
+  }`;
+}
