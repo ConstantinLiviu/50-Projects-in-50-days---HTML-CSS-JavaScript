e1afade2175c3c7566a5113a4091a6b1826b786f
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-14 21:12:12 +0200

    added functionality

--- Day 44 - Custom Range Slider/script.js
+++ Day 44 - Custom Range Slider/script.js
@@ -1,1 +1,15 @@
-alert("merge!");
+const inputEl = document.getElementById("range");
+const labelEl = document.querySelector("label");
+
+inputEl.addEventListener("input", () => {
+  labelEl.textContent = inputEl.value;
+  labelEl.style.marginLeft = `${inputEl.value}%`;
+  //   lazy solution to mostly center the label, especially at both ends of the range
+  if (+inputEl.value < 10) {
+    labelEl.style.transform = `translateX(-37%)`;
+  } else if (+inputEl.value == 100) {
+    labelEl.style.transform = `translateX(-66%)`;
+  } else {
+    labelEl.style.transform = `translateX(-50%)`;
+  }
+});
