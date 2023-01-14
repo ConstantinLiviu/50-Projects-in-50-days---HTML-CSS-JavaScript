ca94fc6cfaf5dffd003659ea9db9bd7cc8e39019
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-14 23:41:08 +0200

    navbar adjested to vertical display

--- Day 45 - Netflix-ish Navigation/README.md
+++ Day 45 - Netflix-ish Navigation/README.md
@@ -3,9 +3,8 @@
     Version 1.0:
     The name of the project can be misleading as there is no technical similarity between the Netflix page and this project. This particular project displays a triple layered navbar that uses the netflix color scheme. It works both on mobile and desktop and includes a few mock menu options.
 
-
-
     Project milestones are:
+     ✔  adjust project navbar for vertical display;
     create page structure;
     add styles;
     responsive design;
--- Day 45 - Netflix-ish Navigation/script.js
+++ Day 45 - Netflix-ish Navigation/script.js
@@ -1,1 +1,15 @@
-alert("merge!");
+// Navigation Basic Functionality
+
+const menuBtnEl = document.querySelector(".menu-btn");
+const menuCloseEl = document.querySelector(".menu-close");
+const navigationSection = document.querySelector(".navigation");
+
+menuBtnEl.addEventListener("click", () => {
+  navigationSection.classList.remove("hidden");
+});
+
+menuCloseEl.addEventListener("click", () => {
+  navigationSection.classList.add("hidden");
+});
+
+// ////////////////////////////////////////////////////////////////////////
