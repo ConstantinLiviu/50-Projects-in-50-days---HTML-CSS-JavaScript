e42a7641dcc2a3e2e9a00a4f0aca30aaa80f5090
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-18 00:34:33 +0200

    minor fix

--- Day 33 - Notes App/script.js
+++ Day 33 - Notes App/script.js
@@ -1,12 +1,13 @@
 // change add note button styling on mobile devices
 const addNoteBtn = document.querySelector(".add-note-btn");
 
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  if (window.scrollY > 100) {
-    addNoteBtn.classList.add("scrolled");
-  } else {
-    addNoteBtn.classList.remove("scrolled");
-  }
-});
+if (screen.width < 701) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+      addNoteBtn.classList.add("scrolled");
+    } else {
+      addNoteBtn.classList.remove("scrolled");
+    }
+  });
+}
 ///////////////////////////////////////////////////////////////
