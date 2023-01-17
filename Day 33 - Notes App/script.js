1ce13387440aede92a41533c4a97a266824efe1a
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-18 00:21:18 +0200

    project redo, added btn styling change for mobile

--- Day 33 - Notes App/script.js
+++ Day 33 - Notes App/script.js
@@ -1,1 +1,12 @@
-alert("merge!");
+// change add note button styling on mobile devices
+const addNoteBtn = document.querySelector(".add-note-btn");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 100) {
+    addNoteBtn.classList.add("scrolled");
+  } else {
+    addNoteBtn.classList.remove("scrolled");
+  }
+});
+///////////////////////////////////////////////////////////////
