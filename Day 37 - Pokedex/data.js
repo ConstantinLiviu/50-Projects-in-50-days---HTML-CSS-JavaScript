8eb9a5a429aed1d1b7a957d9cbfb2dd30312fca1
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-20 00:54:09 +0200

    added cards' background color collection, exported module

--- Day 37 - Pokedex/data.js
+++ Day 37 - Pokedex/data.js
@@ -1,3 +1,18 @@
-const cardsBg = [];
-const typeColor = [];
-const elementIcons = [];
+export const cardsBg = {
+  fire: "#FD8A8A",
+  grass: "#CEEDC7",
+  electric: "#FFF89A",
+  water: "#B8E8FC",
+  ground: "#D0B8A8",
+  rock: "#D0C9C0",
+  fairy: "#FFBCD1",
+  poison: "#03C988",
+  bug: "#FEC868",
+  dragon: "#7766ee",
+  psychic: "#FF6E31",
+  flying: "#2B3467",
+  fighting: "#434242",
+  normal: "#F3EFE0",
+};
+export const typeColor = [];
+export const elementIcons = [];
