e6d76f38ab608885f9f8716ef510aa9ea86826bb
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-24 23:53:27 +0200

    added functionality

--- Day 43 - Feedback UI Design/README.md
+++ Day 43 - Feedback UI Design/README.md
@@ -1,14 +1,14 @@
 # 43 - FEEDBACK UI DESIGN
 
     Version 1.0:
-    A simple feedback app that allows users to express their satisfaction level. There are 3 choices and a confirmation message is displayed once a choice is made.
+    A simple feedback app that allows users to express their satisfaction level. There are 3 choices and a confirmation message is displayed once a choice is made. This project makes use of event bubbling.
 
     Project milestones are:
      ✔  create page structure;
      ✔  add styling;
      ✔  responsive design;
-    add functionality;
-    refactor code;
+     ✔  add functionality;
+     ✔  refactor code;
     add thumbnail;
 
     Status:
--- Day 43 - Feedback UI Design/script.js
+++ Day 43 - Feedback UI Design/script.js
@@ -1,1 +1,39 @@
-alert("merge!");
+const reviewOptions = document.querySelectorAll(".review");
+const submitBtn = document.querySelector(".submit span");
+const reviewsContainer = document.querySelector(".submit-review-container");
+const confirmationContainer = document.querySelector(".confirmation-container");
+const feedbackValue = document.querySelector(".feedback-value span");
+let userChoice;
+
+reviewsContainer.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("review")) {
+    reviewOptions.forEach((review) => {
+      review.classList.remove("active");
+    });
+    e.target.parentNode.classList.add("active");
+    userChoice = e.target.nextElementSibling.textContent;
+  }
+});
+
+submitBtn.addEventListener("click", () => {
+  if (userChoice) {
+    reviewsContainer.classList.toggle("hide");
+    confirmationContainer.classList.toggle("hide");
+    feedbackValue.textContent = userChoice;
+    if (userChoice === "Unhappy") {
+      feedbackValue.style.color = "#FF0000";
+    } else if (userChoice === "Neutral") {
+      feedbackValue.style.color = "#A35638";
+    } else {
+      feedbackValue.style.color = "#249101";
+    }
+  } else {
+    alert(
+      "Please choose one of the options best representing the service you've received"
+    );
+  }
+  setTimeout(() => {
+    reviewsContainer.classList.toggle("hide");
+    confirmationContainer.classList.toggle("hide");
+  }, 2000);
+});
