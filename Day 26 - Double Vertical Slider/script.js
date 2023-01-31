3ea8b077df12c261881c8201e1ca8b9aa85e0a79
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-01-31 23:49:47 +0200

    created selectors, project is on hold for the time being

--- Day 26 - Double Vertical Slider/script.js
+++ Day 26 - Double Vertical Slider/script.js
@@ -1,1 +1,13 @@
-alert("merge!");
+const projectContainerEl = document.querySelector(".project-container");
+const textSliderEl = document.querySelector(".text-slider");
+const textSliderItems = textSliderEl.querySelectorAll(".text-slider-item");
+const imgSliderEl = document.querySelector(".img-slider");
+const nextBtnEl = document.querySelector(".next");
+const prevBtnEl = document.querySelector(".prev");
+const slidesLength = imgSliderEl.querySelectorAll(".img-slider-item").length;
+
+console.log(slidesLength);
+
+let slideInView = 0;
+
+nextBtnEl.addEventListener("click", () => {});
