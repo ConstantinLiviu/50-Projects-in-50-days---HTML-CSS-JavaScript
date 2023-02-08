dd6a47ed26b48d982c17c38c803843a9f6354ea2
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-02-08 23:57:31 +0200

    enabled auto loop on mobile

--- Day 35 - Image Carousel/script.js
+++ Day 35 - Image Carousel/script.js
@@ -2,15 +2,24 @@ const carouselEl = document.querySelector(".carousel");
 const prevBtn = document.querySelector(".previous");
 const nextBtn = document.querySelector(".next");
 
+let step;
+if (window.innerWidth > 768) {
+  step = 660;
+} else {
+  step = 340;
+}
+
 let i = 0;
-let timer = setInterval(carouselLoopDesktop, 2000);
+let timer = setInterval(function () {
+  carouselLoopDesktop(step);
+}, 2000);
 
 /**
  * Function scrolls carousel container and increases/resets index
  */
 
-function carouselLoopDesktop() {
-  carouselEl.scrollLeft += 660;
+function carouselLoopDesktop(step) {
+  carouselEl.scrollLeft += step;
   i++;
   if (i > 3) {
     i = 0;
