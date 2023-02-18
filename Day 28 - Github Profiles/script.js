01e6e1401c3ed15c4ad73e09a1e2aba5726a4d6f
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-02-18 23:56:40 +0200

    changed structure, adjusted styling, added search functionality

--- Day 28 - Github Profiles/script.js
+++ Day 28 - Github Profiles/script.js
@@ -1,6 +1,8 @@
 const APIURL = "https://api.github.com/users/";
 const formEl = document.querySelector("form");
 const searchEl = document.querySelector(".searchbar");
+const projectContainerEl = document.querySelector(".project-container");
+const profileInfoEl = document.querySelector(".profile-info-container");
 
 /**
  * Function takes in the username in order to return info from their Github profile
@@ -11,13 +13,55 @@ async function getUser(username) {
     // destructure response object to get just the 'data' property
     const { data } = await axios(APIURL + username);
 
-    console.log(data);
+    userProfile(data);
   } catch (err) {
-    console.log(err);
+    if (err.response.status == 404) {
+      noMatch("No matches found :(");
+    }
   }
 }
 
-function createUserCard(user) {}
+function userProfile(user) {
+  if (!projectContainerEl.classList.contains("expand")) {
+    projectContainerEl.classList.add("expand");
+  }
+
+  profileInfoEl.innerHTML = "";
+  profileInfoEl.classList.add("fade");
+
+  profileInfoEl.innerHTML = `<div class="img-container">
+    <img
+      src="${user.avatar_url}"
+      alt="${user.name} profile-pic"
+    />
+  </div>
+  <div class="profile-info">
+    <h1 class="name">${user.name}</h1>
+    <p class="bio">
+      ${user.bio}
+    </p>
+    <div class="stats">
+      <p>${user.followers} <strong>Followers</strong></p>
+      <p>${user.following} <strong>Following</strong></p>
+      <p>${user.public_repos} <strong>Repositories</strong></p>
+    </div>
+    <div class="repos">
+    </div>
+  </div>`;
+  setTimeout(() => {
+    profileInfoEl.classList.remove("fade");
+  }, 200);
+}
+
+function noMatch(msg) {
+  const cardHTML = document.createElement("div");
+  cardHTML.innerHTML = `<div>
+  <h1>
+  ${msg}
+  </h1>  
+  </div>`;
+  projectContainerEl.appendChild(cardHTML);
+}
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,6 +69,7 @@ formEl.addEventListener("submit", (e) => {
   const user = searchEl.value;
 
   if (user) {
+    // projectContainerEl.classList.remove("expand");
     getUser(user);
 
     searchEl.value = "";
