ec72d8bebfea96417bd6678884bd6cd549d0d18f
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-02-19 14:34:34 +0200

    added functionality - retrieve and display latest (5) repositories w/ access link

--- Day 28 - Github Profiles/README.md
+++ Day 28 - Github Profiles/README.md
@@ -9,7 +9,7 @@
      ✔  add styling;
      ✔  responsive design;
      ✔  add functionality;
-    refactor code;
+     ✔  refactor code;
     add thumbnail;
 
     Status:
--- Day 28 - Github Profiles/script.js
+++ Day 28 - Github Profiles/script.js
@@ -6,7 +6,7 @@ const profileInfoEl = document.querySelector(".profile-info-container");
 
 /**
  * Function takes in the username in order to return info from their Github profile
- * @param {string} username
+ * @param {string} username the string input by the user when looking up Github profiles
  */
 async function getUser(username) {
   try {
@@ -14,6 +14,7 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username);
 
     userProfile(data);
+    showRepo(username);
   } catch (err) {
     if (err.response.status == 404) {
       noMatch("No matches found :(");
@@ -21,6 +22,23 @@ async function getUser(username) {
   }
 }
 
+/**
+ * Function uses repositories Github link to retrieve repositories and sort them by date of release and calls a function to create and display info on page
+ * @param {string} username the string input by the user when looking up Github profiles
+ */
+async function showRepo(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created");
+    addRepos(data);
+  } catch (err) {
+    noMatch("unable to retrieve repositories :(");
+  }
+}
+
+/**
+ * Function retrieves Github profile data, expands profile info field and creates the HTML elements
+ * @param {URL} user  the API link/username concatenation used to retrieve Github profile data
+ */
 function userProfile(user) {
   if (!projectContainerEl.classList.contains("expand")) {
     projectContainerEl.classList.add("expand");
@@ -53,6 +71,10 @@ function userProfile(user) {
   }, 200);
 }
 
+/**
+ * Function creates HTML elements in order to display an error response if one is enncountered
+ * @param {string} msg preset string representing the encountered error
+ */
 function noMatch(msg) {
   if (!projectContainerEl.classList.contains("expand")) {
     projectContainerEl.classList.add("expand");
@@ -64,13 +86,29 @@ function noMatch(msg) {
   </div>`;
 }
 
+/**
+ *  Function retireves all available info regarding existing repositories, creates an element for each and appends it to an existing HTML element while limiting the displayed elements to 5, regardless of the length of the repositories list
+ * @param {URL} repos Github API link to gain access to repositories info
+ */
+function addRepos(repos) {
+  const reposEl = document.querySelector(".repos");
+
+  repos.slice(0, 5).forEach((repo) => {
+    const repoEl = document.createElement("a");
+    repoEl.classList.add("repo");
+    repoEl.href = repo.html_url;
+    repoEl.target = "_blank";
+    repoEl.textContent = repo.name;
+    reposEl.appendChild(repoEl);
+  });
+}
+
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const user = searchEl.value;
 
   if (user) {
-    // projectContainerEl.classList.remove("expand");
     getUser(user);
 
     searchEl.value = "";
