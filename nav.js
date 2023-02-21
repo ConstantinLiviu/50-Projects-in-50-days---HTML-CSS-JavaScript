30751c5491b4c9fb109a879c5fb217e2c5a543a2
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-02-21 23:27:30 +0200

    enabled random project redirect on main page

--- nav.js
+++ nav.js
@@ -1,6 +1,14 @@
+import { randomProjectsArr } from "./random-project-links-array.js";
+
 const homeMenuIcon = document.querySelector(".ham-menu");
 const navList = document.querySelector("nav ul");
+const randomProjectEl = document.querySelector("nav ul li:last-child a");
 
 homeMenuIcon.addEventListener("click", () => {
   navList.classList.toggle("hide-ul");
+  console.log(randomProjectsArr);
+});
+
+randomProjectEl.addEventListener("click", () => {
+  randomProjectEl.href = `${randomProjectsArr[Math.floor(Math.random() * 51)]}`;
 });
