096648ace97196620965960c5016350530f3ab25
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-02-22 23:24:44 +0200

    added digit animation

--- Day 34 - Animated Countdown/script.js
+++ Day 34 - Animated Countdown/script.js
@@ -1,1 +1,22 @@
-alert("merge!");
+const numArr = document.querySelectorAll(".nums-container p");
+const counterEl = document.querySelector(".counter-container");
+const countdownMsg = document.querySelector(".prep-msg");
+const restartBtn = document.querySelector(".restart");
+
+startCountdown();
+
+function startCountdown() {
+  numArr.forEach((num, i) => {
+    const nextToLast = numArr.length - 1;
+    num.addEventListener("animationend", (e) => {
+      if (e.animationName === "count" && i !== nextToLast) {
+        num.classList.remove("active");
+        num.classList.add("counted");
+      } else if (e.animationName === "countOut" && num.nextElementSibling) {
+        num.nextElementSibling.classList.add("active");
+      }
+    });
+  });
+}
+
+// function newCountdown() {}
