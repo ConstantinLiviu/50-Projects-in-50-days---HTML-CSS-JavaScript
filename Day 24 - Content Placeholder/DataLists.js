fe5eb64e836ce9e8e095540cb8407ec6327e75cd
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-03-03 00:29:03 +0200

    added project data and linked it to html

--- Day 24 - Content Placeholder/DataLists.js
+++ Day 24 - Content Placeholder/DataLists.js
@@ -1,17 +1,41 @@
-// to be added
-// const imgSrc= [];
+const imgSrc = [
+  "driver.jpg",
+  "engineer.jpg",
+  "manager.jpg",
+  "police.jpg",
+  "tennis.jpg",
+];
 
-// to be added
-// const profileImgSrc= [];
+const profileImgSrc = [
+  "https://randomuser.me/api/portraits/men/79.jpg",
+  "https://randomuser.me/api/portraits/men/96.jpg",
+  "https://randomuser.me/api/portraits/men/26.jpg",
+  "https://randomuser.me/api/portraits/men/44.jpg",
+  "https://randomuser.me/api/portraits/men/70.jpg",
+];
 
-// to be added
-// const cardTitles = [];
+const cardTitles = [
+  "Speed and safety is the name of the game",
+  "There are no problems, only puzzles to be solved",
+  "Seeing the big picture is the key to success",
+  "Making everyone feel save is no small feat",
+  "Go big or go home!",
+];
 
-// to be added
-// const cardSubTitles = []
+const cardSubTitles = [
+  "When you're in a hurry you want things done fast. But one must not get reckless in the process",
+  "Each situation has its own challenges. That's what we do here, figure out the best way to deal with it.",
+  "There are no two days alike. One has to be bold and have a sharp mind.",
+  "In order to keep the community safe you must be one with the community",
+  "This isn't a sport, it's a battle! And it's not for the faint of heart!",
+];
 
-// to be added
-// const names = []
+const names = [
+  "Michael Johnson",
+  "Donovan Campbell",
+  "Tyrone Williams",
+  "Arnold Davis",
+  "John Brown",
+];
 
-// to be added
-// const occupation = []
+const occupation = ["Driver", "Engineer", "Manager", "Police", "Tennis player"];
