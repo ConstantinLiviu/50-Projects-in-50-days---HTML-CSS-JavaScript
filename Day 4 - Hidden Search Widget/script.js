bafb941d4cf4d6a432990d2fd17ed0b6950dafa2
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-03-11 23:57:16 +0200

    mobile navbar menu button fix

--- Day 4 - Hidden Search Widget/script.js
+++ Day 4 - Hidden Search Widget/script.js
@@ -1,11 +1,6 @@
 const searchBtn = document.querySelector("button");
 const inputEl = document.querySelector("input");
-const homeMenuIcon = document.querySelector(".ham-menu");
 const navList = document.querySelector("nav ul");
-
-homeMenuIcon.addEventListener("click", () => {
-  navList.classList.toggle("hide-ul");
-});
 
 searchBtn.addEventListener("click", () => {
   inputEl.classList.toggle("active");
