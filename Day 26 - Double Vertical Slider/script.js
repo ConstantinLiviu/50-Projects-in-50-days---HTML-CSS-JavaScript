dee0dad3afe590b273eaa820228eb8a7730bb8fa
MaFomedanu <46496235+ConstantinLiviu@users.noreply.github.com>
2023-03-23 00:36:56 +0200

    next btn + transition functional

--- Day 26 - Double Vertical Slider/script.js
+++ Day 26 - Double Vertical Slider/script.js
@@ -1,11 +1,18 @@
-const projectContainerEl = document.querySelector(".project-container");
-const textSliderEl = document.querySelector(".text-slider");
-const textSliderItems = textSliderEl.querySelectorAll(".text-slider-item");
-const imgSliderEl = document.querySelector(".img-slider");
+const imgContainerEl = document.querySelector(".img-container");
 const nextBtnEl = document.querySelector(".next");
 const prevBtnEl = document.querySelector(".prev");
-const slidesLength = imgSliderEl.querySelectorAll(".img-slider-item").length;
 
-let slideInView = 0;
+nextBtnEl.addEventListener("click", () => {
+  imgContainerEl.style.transform = "translateY(-100vh)";
+});
 
-nextBtnEl.addEventListener("click", () => {});
+prevBtnEl.addEventListener("click", () => {});
+
+imgContainerEl.addEventListener("transitionend", () => {
+  imgContainerEl.append(imgContainerEl.firstElementChild);
+  imgContainerEl.style.transition = "none";
+  imgContainerEl.style.transform = "translateY(0)";
+  setTimeout(() => {
+    imgContainerEl.style.transition = "all 0.6s ease";
+  }, 10);
+});
